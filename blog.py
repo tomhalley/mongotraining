10c921856dfd25ae7e9ab436e42df4e10511e7d1
tomhalley89 <tomhalley89@gmail.com>
2012-10-25 17:06:41 +0100

    Converted to virtual environment

---
+++ blog.py
@@ -0,0 +1,39 @@
+import bottle
+from bottle import route, request, static_file, template, view
+import pymongo
+from pymongo import Connection
+
+connection = Connection('localhost', 27017)
+db = connection.test
+
+
+
+@route('/')
+@view('views/index.tpl')
+def index():
+	blog = db.blog
+	return dict(posts=blog.find())
+
+
+
+@route('/comment', method='POST')
+def submit_comment():
+	return "Thanks!";
+
+
+
+@route('/contact')
+@view('views/contact.tpl')
+def contact():
+	return "Thanks!"
+
+
+
+@route('/static/<filename>', name='static')
+def server_static(filename):
+	return static_file(filename, root='static')
+
+
+
+
+bottle.run(host='localhost', port=8082)
---
+++ views/contact.tpl~
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Blog</title>
+		<link rel="stylesheet" type="text/css" href="static/main.css" />
+	</head>
+	<body>
+	</body>
+</html>
